4cc71425fd951e3fc1b9e57076c084b94d4a4594
Grzegorz Bokota <bokota+github@gmail.com>
2023-07-09 22:44:44 +0200

    refactor script for adding logins to CITATION.cff

--- add_login_to_citation_cff.py
+++ add_login_to_citation_cff.py
@@ -1,4 +1,12 @@
+"""
+This is script for adding logins to existing CITATION.cff file. 
+This simplify future updating of this file. Its create bakup file with .bck suffix/
+"""
+from __future__ import annotations
+
 import argparse
+import shutil
+import sys
 from pathlib import Path
 
 from yaml import safe_dump, safe_load
@@ -8,32 +16,40 @@ from unidecode import unidecode
 from release_utils import get_repo, setup_cache, BOT_LIST
 
 LOCAL_DIR = Path(__file__).parent
-
-parser = argparse.ArgumentParser()
-parser.add_argument("path", help="The path to the citation file to sort", type=Path)
-parser.add_argument(
-    "--correction-file",
-    help="The file with the corrections",
-    default=LOCAL_DIR / "name_corrections.yaml",
-)
-args = parser.parse_args()
-
-setup_cache()
+DEFAULT_CORRECTION_FILE = LOCAL_DIR / "name_corrections.yaml"
 
 
-with args.path.open(encoding="utf8") as f:
-    data = safe_load(f)
+def existing_file(value):
+    file_path = Path(value)
+    if not file_path.exists():
+        raise argparse.ArgumentTypeError(f"The file {file_path} does not exists.")
+    return file_path
 
-contributors_iterable = get_repo().get_contributors()
+def get_correction_dict(file_path: Path | None) -> dict[str, str]:
+    """
+    Read file with correction of name between 
+    """
+    if not file_path or not file_path.exists(): 
+        return {}
 
-correction_dict = {}
-with open(args.correction_file) as f:
-    corrections = safe_load(f)
-    for correction in corrections["login_to_name"]:
-        correction_dict[correction["login"]] = unidecode(correction["corrected_name"].lower())
+    correction_dict = {}
+    with open(file_path) as f:
+        corrections = safe_load(f)
+        for correction in corrections["login_to_name"]:
+            correction_dict[correction["login"]] = unidecode(correction["corrected_name"].lower())
+    
+    return correction_dict
 
 
-def get_name(user):
+def get_correctrions_from_citation_cff(cff_dict):
+    res = {}
+    for author in cff_dict["authors"]:
+        if "alias" in author:
+            res[author["alias"]] = unidecode(f'{author["given-names"]} {author["family-names"]}'.lower())
+    return res 
+
+
+def get_name(user, correction_dict):
     if user.login in correction_dict:
         return correction_dict[user.login]
     if user.name is None:
@@ -41,26 +57,52 @@ def get_name(user):
     return unidecode(user.name.lower())
 
 
-contributors = { get_name(user): user for user in
-    tqdm(contributors_iterable, total=contributors_iterable.totalCount) if get_name(user) is not None
-}
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("path", help="The path to the citation file to sort", type=Path)
+    parser.add_argument(
+        "--correction-file",
+        help="The file with the corrections",
+        default=DEFAULT_CORRECTION_FILE,
+        type=existing_file,
+    )
+    args = parser.parse_args()
 
-for user in get_repo().get_contributors():
-    if get_name(user) is None  and user.login not in BOT_LIST:
-        print(f"Could not find {user.login}")
+def add_logins(cff_path: Path, correction_file: Path | None = None) -> None:
+    setup_cache()
 
-# assert len(contributors) == contributors_iterable.totalCount
+    with cff_path.open(encoding="utf8") as f:
+        data = safe_load(f)
+
+    contributors_iterable = get_repo().get_contributors()
+
+    correction_dict = get_correction_dict(correction_file) | get_correctrions_from_citation_cff(data)
+
+    contributors = { get_name(user, correction_dict): user for user in
+        tqdm(contributors_iterable, total=contributors_iterable.totalCount) if get_name(user) is not None
+    }
+
+    for user in get_repo().get_contributors():
+        if get_name(user) is None  and user.login not in BOT_LIST:
+            print(f"Could not find {user.login}", file=sys.stderr)
+
+    # assert len(contributors) == contributors_iterable.totalCount
 
 
-for i, author in enumerate(data["authors"]):
-    if "alias" in author:
-        continue
-    name = unidecode(f'{author["given-names"]} {author["family-names"]}'.lower())
-    if name in contributors:
-        author["alias"] = contributors[name].login
-    else:
-        print(f"Could not find {name}")
+    for i, author in enumerate(data["authors"]):
+        if "alias" in author:
+            continue
+        name = unidecode(f'{author["given-names"]} {author["family-names"]}'.lower())
+        if name in contributors:
+            author["alias"] = contributors[name].login
+        else:
+            print(f"Could not find {name}", file=sys.stderr)
+
+    shutil.copy(str(cff_path), f"{cff_path}.bck")
+
+    with cff_path.open("w", encoding="utf8") as f:
+        safe_dump(data, f, sort_keys=False, allow_unicode=True)
 
 
-with args.path.open("w", encoding="utf8") as f:
-    safe_dump(data, f, sort_keys=False, allow_unicode=True)
+if __name__ == "__main__":
+    main()
