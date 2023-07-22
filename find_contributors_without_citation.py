70c31301f1d209555388beb3ad1962805352ef95
Grzegorz Bokota <bokota+github@gmail.com>
2023-07-22 19:03:15 +0200

    update script for looking for missing autors

--- find_contributors_without_citation.py
+++ find_contributors_without_citation.py
@@ -7,83 +7,76 @@ from release_utils import (
     get_milestone,
     iter_pull_request,
     setup_cache,
+    BOT_LIST,
 )
 
 LOCAL_DIR = Path(__file__).parent
 
-parser = argparse.ArgumentParser()
-parser.add_argument("milestone", help="The milestone to check")
-parser.add_argument(
-    "--correction-file",
-    help="The file with the corrections",
-    default=LOCAL_DIR / "name_corrections.yaml",
-)
-parser.add_argument(
-    "--citation-path",
-    help="",
-    default=LOCAL_DIR / "napari_repo" / "CITATION.cff",
-    type=Path,
-)
-parser.add_argument(
-    "--generate",
-    help="Generate the missing entries based on github data",
-    action="store_true",
-)
 
-args = parser.parse_args()
+def existing_file(path: str) -> Path:
+    path = Path(path)
+    if not path.exists():
+        raise FileNotFoundError(f"{path} not found")
+    return path
 
 
-if not args.citation_path.exists():
-    raise FileNotFoundError("Citation file not found")
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("milestone", help="The milestone to check")
+    parser.add_argument(
+        "--correction-file",
+        help="The file with the corrections",
+        default=LOCAL_DIR / "name_corrections.yaml",
+    )
+    parser.add_argument(
+        "--citation-path",
+        help="",
+        default=str(LOCAL_DIR / "napari_repo" / "CITATION.cff"),
+        type=existing_file,
+    )
+    parser.add_argument(
+        "--generate",
+        help="Generate the missing entries based on github data",
+        action="store_true",
+    )
 
-with args.citation_path.open() as f:
-    citation = safe_load(f)
+    args = parser.parse_args()
+    with args.citation_path.open() as f:
+        citation = safe_load(f)
 
-author_dict = {}
+    missing_authors = find_missing_authors(citation)
 
-for author in citation["authors"]:
-    author_dict[f"{author['given-names']} {author['family-names']}"] = author
-
-correction_dict = {}
-with open(args.correction_file) as f:
-    corrections = safe_load(f)
-    for correction in corrections["login_to_name"]:
-        correction_dict[correction["login"]] = correction["corrected_name"]
-
-
-setup_cache()
-
-milestone = get_milestone(args.milestone)
-
-missing_authors = set()
+    if args.generate:
+        for login, name in sorted(missing_authors):
+            if name is None:
+                continue
+            name, sure_name = name.rsplit(" ", 1)
+            print(f"- given-names: {name}\n  family-names: {sure_name}\n alias: {login}")
+    else:
+        for login, name in sorted(missing_authors):
+            print(f"@{login} ", end="")  # ({name})")
+    print()
 
 
-for pull in iter_pull_request(f"milestone:{args.milestone} is:merged"):
-    issue = pull.as_issue()
-    creator = issue.user
-    if creator.login in {
-        "github-actions[bot]",
-        "pre-commit-ci[bot]",
-        "dependabot[bot]",
-        "napari-bot",
-    }:
-        continue
-    if correction_dict.get(creator.login, creator.name) not in author_dict:
-        missing_authors.add((creator.login, creator.name))
+def find_missing_authors(citation, milestone_str: str):
+    author_dict = {}
 
-if args.generate:
-    for login, name in sorted(missing_authors):
-        if name is None:
+    for author in citation["authors"]:
+        author_dict[author['alias']] = author
+
+    setup_cache()
+    milestone = get_milestone(milestone_str)
+    missing_authors = set()
+
+    for pull in iter_pull_request(f"milestone:{milestone.title} is:merged"):
+        issue = pull.as_issue()
+        creator = issue.user
+        if creator.login in BOT_LIST:
             continue
-        name, sure_name = name.rsplit(" ", 1)
-        print(
-            f"""- given-names: {name}
-  family-names: {sure_name}
-""",
-            end="",
-        )
-else:
-    for login, name in sorted(missing_authors):
-        print(f"@{login} ", end="")  # ({name})")
+        if creator.login not in author_dict:
+            missing_authors.add((creator.login, creator.name))
+    return missing_authors
 
-print()
+
+if __name__ == "__main__":
+    main()
