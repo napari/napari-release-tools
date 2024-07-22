5835116f0d5de74140c46f33ba5e2c35148e7bc6
Juan Nunez-Iglesias <jni@fastmail.com>
2024-07-22 23:41:39 +1000

    Use ruff-fmt instead of black

--- generate_release_notes.py
+++ generate_release_notes.py
@@ -1,4 +1,5 @@
 """Generate the release notes automatically from GitHub pull requests.
+
 Start with:
 ```
 export GH_TOKEN=<your-gh-api-token>
@@ -24,6 +25,7 @@ https://github.com/scikit-image/scikit-image/blob/main/tools/generate_release_no
 https://github.com/scikit-image/scikit-image/issues/3404
 https://github.com/scikit-image/scikit-image/issues/3405
 """
+
 import argparse
 import re
 import sys
@@ -49,7 +51,7 @@ from release_utils import (
 
 LOCAL_DIR = Path(__file__).parent
 
-PR_REGEXP = re.compile(r"(?P<user>[\w-]+)/(?P<repo>[\w-]+)#(?P<pr>\d+)")
+PR_REGEXP = re.compile(r'(?P<user>[\w-]+)/(?P<repo>[\w-]+)#(?P<pr>\d+)')
 
 
 class PRInfo(NamedTuple):
@@ -60,27 +62,29 @@ class PRInfo(NamedTuple):
 
 def parse_pr_num(pr_num):
     if match := PR_REGEXP.match(pr_num):
-        return PRInfo(match.group("user"), match.group("repo"), int(match.group("pr")))
+        return PRInfo(
+            match.group('user'), match.group('repo'), int(match.group('pr'))
+        )
     try:
         return int(pr_num)
     except ValueError:
-        raise argparse.ArgumentTypeError(f"{pr_num} is not a valid PR number.")
+        raise argparse.ArgumentTypeError(f'{pr_num} is not a valid PR number.')
 
 
 parser = argparse.ArgumentParser(usage=__doc__)
-parser.add_argument("milestone", help="The milestone to list")
-parser.add_argument("--target-directory", type=Path, default=None)
+parser.add_argument('milestone', help='The milestone to list')
+parser.add_argument('--target-directory', type=Path, default=None)
 parser.add_argument(
-    "--correction-file",
-    help="The file with the corrections",
-    default=LOCAL_DIR / "name_corrections.yaml",
+    '--correction-file',
+    help='The file with the corrections',
+    default=LOCAL_DIR / 'name_corrections.yaml',
 )
 parser.add_argument(
-    "--with-pr",
-    help="Include PR numbers for not merged PRs",
+    '--with-pr',
+    help='Include PR numbers for not merged PRs',
     type=parse_pr_num,
     default=None,
-    nargs="+",
+    nargs='+',
 )
 
 args = parser.parse_args()
@@ -90,7 +94,9 @@ setup_cache()
 repo = get_repo()
 correction_dict = get_correction_dict(
     args.correction_file
-) | get_corrections_from_citation_cff(LOCAL_DIR / REPO_DIR_NAME / "CITATION.cff")
+) | get_corrections_from_citation_cff(
+    LOCAL_DIR / REPO_DIR_NAME / 'CITATION.cff'
+)
 
 
 def add_to_users(users_dkt, new_user):
@@ -114,30 +120,30 @@ docs_reviewers = set()
 users = {}
 
 highlights = {
-    "Highlights": {},
-    "New Features": {},
-    "Improvements": {},
-    "Performance": {},
-    "Bug Fixes": {},
-    "API Changes": {},
-    "Deprecations": {},
-    "Build Tools": {},
-    "Documentation": {},
+    'Highlights': {},
+    'New Features': {},
+    'Improvements': {},
+    'Performance': {},
+    'Bug Fixes': {},
+    'API Changes': {},
+    'Deprecations': {},
+    'Build Tools': {},
+    'Documentation': {},
 }
 
 other_pull_requests = {}
 
 label_to_section = {
-    "bug": "Bug Fixes",
-    "bugfix": "Bug Fixes",
-    "feature": "New Features",
-    "api": "API Changes",
-    "highlight": "Highlights",
-    "performance": "Performance",
-    "enhancement": "Improvements",
-    "deprecation": "Deprecations",
-    "dependencies": "Build Tools",
-    "documentation": "Documentation",
+    'bug': 'Bug Fixes',
+    'bugfix': 'Bug Fixes',
+    'feature': 'New Features',
+    'api': 'API Changes',
+    'highlight': 'Highlights',
+    'performance': 'Performance',
+    'enhancement': 'Improvements',
+    'deprecation': 'Deprecations',
+    'dependencies': 'Build Tools',
+    'documentation': 'Documentation',
 }
 
 
@@ -164,19 +170,19 @@ def parse_pull(pull: PullRequest, repo_: Repository = repo):
     for label_name, section in label_to_section.items():
         if label_name in pr_labels:
             highlights[section][pull.number] = {
-                "summary": summary,
-                "repo": repo_.full_name.split("/")[1],
+                'summary': summary,
+                'repo': repo_.full_name.split('/')[1],
             }
             assigned_to_section = True
 
     if not assigned_to_section:
         other_pull_requests[pull.number] = {
-            "summary": summary,
-            "repo": repo_.full_name.split("/")[1],
+            'summary': summary,
+            'repo': repo_.full_name.split('/')[1],
         }
 
 
-for pull_ in iter_pull_request(f"milestone:{args.milestone} is:merged"):
+for pull_ in iter_pull_request(f'milestone:{args.milestone} is:merged'):
     parse_pull(pull_)
 
 if args.with_pr is not None:
@@ -191,7 +197,7 @@ if args.with_pr is not None:
         parse_pull(pull, r)
 
 for pull in iter_pull_request(
-    f"milestone:{args.milestone} is:merged", repo=GH_DOCS_REPO
+    f'milestone:{args.milestone} is:merged', repo=GH_DOCS_REPO
 ):
     issue = pull.as_issue()
     assert pull.merged
@@ -207,17 +213,20 @@ for pull in iter_pull_request(
             docs_reviewers.add(review.user.login)
     assigned_to_section = False
     pr_labels = {label.name.lower() for label in pull.labels}
-    if "maintenance" in pr_labels:
-        other_pull_requests[pull.number] = {"summary": summary, "repo": GH_DOCS_REPO}
+    if 'maintenance' in pr_labels:
+        other_pull_requests[pull.number] = {
+            'summary': summary,
+            'repo': GH_DOCS_REPO,
+        }
     else:
-        highlights["Documentation"][pull.number] = {
-            "summary": summary,
-            "repo": GH_DOCS_REPO,
+        highlights['Documentation'][pull.number] = {
+            'summary': summary,
+            'repo': GH_DOCS_REPO,
         }
 
 
 # add Other PRs to the ordered dict to make doc generation easier.
-highlights["Other Pull Requests"] = other_pull_requests
+highlights['Other Pull Requests'] = other_pull_requests
 
 
 # remove these bots.
@@ -227,8 +236,8 @@ docs_committers -= BOT_LIST
 docs_authors -= BOT_LIST
 
 
-user_name_pattern = re.compile(r"@([\w-]+)")  # pattern for GitHub usernames
-pr_number_pattern = re.compile(r"#(\d+)")  # pattern for GitHub PR numbers
+user_name_pattern = re.compile(r'@([\w-]+)')  # pattern for GitHub usernames
+pr_number_pattern = re.compile(r'#(\d+)')  # pattern for GitHub PR numbers
 
 old_contributors = set()
 
@@ -236,8 +245,8 @@ if args.target_directory is None:
     file_handle = sys.stdout
 else:
     res_file_name = f"release_{args.milestone.replace('.', '_')}.md"
-    file_handle = open(args.target_directory / res_file_name, "w")
-    for file_path in args.target_directory.glob("release_*.md"):
+    file_handle = open(args.target_directory / res_file_name, 'w')
+    for file_path in args.target_directory.glob('release_*.md'):
         if file_path.name == res_file_name:
             continue
         with open(file_path) as f:
@@ -245,13 +254,13 @@ else:
 
 
 # Now generate the release notes
-title = f"# napari {args.milestone}"
+title = f'# napari {args.milestone}'
 print(title, file=file_handle)
 
-notes_dir = LOCAL_DIR / "additional_notes" / args.milestone
-if not notes_dir.glob("*.md"):
+notes_dir = LOCAL_DIR / 'additional_notes' / args.milestone
+if not notes_dir.glob('*.md'):
     print(
-        "There is no prepared sections in the additional_notes directory.",
+        'There is no prepared sections in the additional_notes directory.',
         file=sys.stderr,
     )
 
@@ -272,9 +281,12 @@ https://napari.org.
 print(intro, file=file_handle)
 
 for section, pull_request_dicts in highlights.items():
-    print(f"## {section}\n", file=file_handle)
+    print(f'## {section}\n', file=file_handle)
     section_path = (
-        LOCAL_DIR / "additional_notes" / args.milestone / f"{section.lower()}.md"
+        LOCAL_DIR
+        / 'additional_notes'
+        / args.milestone
+        / f'{section.lower()}.md'
     )
     mentioned_pr = set()
     if section_path.exists():
@@ -287,19 +299,19 @@ for section, pull_request_dicts in highlights.items():
     for number, pull_request_info in pull_request_dicts.items():
         if number in mentioned_pr:
             continue
-        repo_str = pull_request_info["repo"]
+        repo_str = pull_request_info['repo']
         repo_prefix = repo_str if repo_str != 'napari' else ''
         print(
             f'- {pull_request_info["summary"]} ([{repo_prefix}#{number}]'
-            f'(https://{GH}/{GH_USER}/{repo_str}/pull/{number}))',
+            f"(https://{GH}/{GH_USER}/{repo_str}/pull/{number}))",
             file=file_handle,
         )
-    print("", file=file_handle)
+    print('', file=file_handle)
 
 
 contributors = {
-    "authors": authors | docs_authors,
-    "reviewers": reviewers | docs_reviewers,
+    'authors': authors | docs_authors,
+    'reviewers': reviewers | docs_reviewers,
 }
 
 # ignore committers
@@ -307,22 +319,24 @@ contributors = {
 new_contributors = (authors | docs_authors) - old_contributors
 
 for section_name, contributor_set in contributors.items():
-    print("", file=file_handle)
+    print('', file=file_handle)
     if None in contributor_set:
         contributor_set.remove(None)
     committer_str = (
-        f"## {len(contributor_set)} {section_name} added to this "
-        "release (alphabetical)"
+        f'## {len(contributor_set)} {section_name} added to this '
+        'release (alphabetical)'
     )
     print(committer_str, file=file_handle)
-    print("", file=file_handle)
-    print("(+) denotes first-time contributors 🥳")
+    print('', file=file_handle)
+    print('(+) denotes first-time contributors 🥳')
 
     for c in sorted(contributor_set, key=lambda x: users[x].lower()):
         if c in authors and c in docs_authors:
             first_repo_name = GH_REPO
-            second_repo_str = (f' ([docs](https://{GH}/{GH_USER}/'
-                               f'{GH_DOCS_REPO}/commits?author={c})) ')
+            second_repo_str = (
+                f' ([docs](https://{GH}/{GH_USER}/'
+                f'{GH_DOCS_REPO}/commits?author={c})) '
+            )
         elif c in authors:
             first_repo_name = GH_REPO
             second_repo_str = ''
@@ -331,8 +345,11 @@ for section_name, contributor_set in contributors.items():
             second_repo_str = ''
 
         first = ' +' if c in new_contributors else ''
-        commit_link = (f"https://{GH}/{GH_USER}/{first_repo_name}/"
-                       f"commits?author={c}")
-        print(f"- [{users[c]}]({commit_link}){second_repo_str} - @{c}{first}",
-              file=file_handle)
-    print("", file=file_handle)
+        commit_link = (
+            f'https://{GH}/{GH_USER}/{first_repo_name}/' f'commits?author={c}'
+        )
+        print(
+            f'- [{users[c]}]({commit_link}){second_repo_str} - @{c}{first}',
+            file=file_handle,
+        )
+    print('', file=file_handle)
